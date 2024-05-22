c6b86e5b3c5739ab4beb05ffaafadb39f7312a24
Chris <Christopherjorge47@icloud.com>
2024-05-22 13:33:46 -0400

    login functionality added

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,28 +1,45 @@
 // Import necessary variables
-import {user} from "../models/user.js"
-import bcrypt from "bcrypt"
+import {user as User} from "../models/user.js";
+import bcrypt from "bcrypt";
 
 
 export const signup = async (req,res) => {
-    const {userName, email, password} = req.body
-    // const name = req.body.name;
-    // const email = req.body.email;
-    // const password = req.body.password
-    const hashedPassword = bcrypt.hashSync(password,10)
+    const {userName, email, password} = req.body;
+    const hashedPassword = bcrypt.hashSync(password,10);
 
     try{
-        const newUser = user.create({
+        const newUser = User.create({
             userName: userName,
             email: email,
             password: hashedPassword
-        })
-        res.sendStatus(200)
+        });
+        return res.sendStatus(200);
     }
     catch(e)
     {
-        console.log(e)
-    }
+        return res.sendStatus(401)
+    };
    
 
-    console.log('New User Created Successfully!')
+    console.log('New User Created Successfully!');
+}
+
+export const login = async (req,res) => {
+    const{userName, password} = req.body;
+
+    const user = await User.findOne({userName});
+    if(!user)
+        {
+            console.log('User could not be found');
+            return res.sendStatus(401);
+        };
+    console.log('User Found!');
+    const validatePassword = bcrypt.compareSync(password, user.password);
+    if(!validatePassword)
+        {
+            console.log('passwords do not match!')
+            return res.sendStatus(401)
+        }
+    res.sendStatus(200)
+    console.log('Passwords Match')
 }
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,6 +5,7 @@ import cors from "cors"
 import {connectToDataBase} from "./config/connection.js"
 import {user} from "./models/user.js"
 import {signup as userSignUp} from "./controllers/userController.js"
+import {login as userLogin} from "./controllers/userController.js"
 
 // Initialize Necessary Variables
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/signup', userSignUp)
+app.post('/login', userLogin)
 app.listen(PORT, () => {
     console.log(`Server listening on port number: ${PORT}`)
 })
