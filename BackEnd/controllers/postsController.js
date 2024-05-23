79e086b096bea01dce24300b65167175a56fd789
Chris <Christopherjorge47@icloud.com>
2024-05-23 17:26:25 -0400

    Modified how posts are created

--- BackEnd/controllers/postsController.js
+++ BackEnd/controllers/postsController.js
@@ -5,11 +5,12 @@ export const create = async(req,res) => {
     {
     console.log('creating post')
     const{userName,title,body,likes} = req.body;
-    const newPost = await Post.create({
+    const newPost ={
         title: title,
         body: body,
         likes: likes
-    })
+    }
+
     console.log('Post Created!')
     const user = await User.findOne({userName})
     console.log(user.posts)
@@ -21,28 +22,6 @@ export const create = async(req,res) => {
             await user.save()
             console.log(user)
         }
-
-    // if(user)
-    //     {
-    //     console.log(`Adding post to user ${user}`)
-    //     user.posts.push(newPost)
-    //     console.log('Post Created!!!')
-    //     return res.sendStatus(200);
-    //     }
-    //     return res.sendStatus(401);
-    // }
-    // catch(error)
-    // {
-    //     console.log('error')
-    //     return res.sendStatus(401);
-    // }
-    
-    // const user = User.findOneAndUpdate({userName},
-    //     {$push : {title: newPost.title, body: newPost.body}}
-
-    // )
-    // console.log(user);
-    // res.sendStatus(201);
     }
     catch(error)
     {
