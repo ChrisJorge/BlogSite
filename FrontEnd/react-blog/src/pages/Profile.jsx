dc759c2179b9b0f9de97b1e4029fdd1196674bc5
Chris <Christopherjorge47@icloud.com>
2024-05-24 18:21:13 -0400

    Button to have edit bio form functional

--- FrontEnd/react-blog/src/pages/Profile.jsx
+++ FrontEnd/react-blog/src/pages/Profile.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { useState, useEffect } from 'react'
-function Profile({user, getInfo, userData}) {
-  
+function Profile({user, getInfo, userData }) {
+  const [visible, setVisible] = useState(false)
+ 
+
+  const modifyVisible = () => {
+    setVisible(!visible)
+  }
+
+  const change = () => {
+    let bio = document.querySelector('.bioTxt')
+    let container = document.querySelector('.bioForm')
+    let txt = document.querySelector('.bioTxt').innerHTML
+    let txtArea = document.querySelector('.bioTextArea');
+    if(visible)
+    {
+     bio.setAttribute('style', 'display: none')
+     container.setAttribute('style', 'display:block')
+     if(txt === 'No bio found, please enter a bio!')
+      {
+       txtArea.placeholder = "Enter a bio!"
+      }
+      else{
+        txtArea.value = txt
+      }
+    }
+    else{
+      bio.setAttribute('style', 'display: block')
+      container.setAttribute('style', 'display:none')
+    }
+
+  }
 
 useEffect(() => {
   getInfo();
 },[]);
 
-  
+useEffect(() => {
+  change()
+},[visible])
+
+const edit = () => {
+  let textval = document.querySelector('.bioTxt').innerHTML
+  let txt = document.querySelector('.bioTxt')
+}
+
+const loaded = () => {
   return (
     <>
       <Navbar user = {user}/>
@@ -21,17 +59,38 @@ useEffect(() => {
       <div className="bioContainer">
         <div className="bioTitle">
           <p className="bioTitleText">Bio</p>
-          <button className='editBioBTN'>
+          <button className='editBioBTN' onClick={modifyVisible}>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"  className="EditBIOSVG" viewBox="0 0 19 19">
                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
             </svg>
           </button>
         </div>
           <div className="bioContent">
+            {userData.bio !== "" ? <p className='bioTxt'>{userData.bio}</p> : <p className='bioTxt'>No bio found, please enter a bio!</p>}
+            <div className="bioForm">
+              <form>
+                <textarea className='bioTextArea'></textarea>
+                <div className="btnContainer">
+                  <button type='reset' className='bioBTN'>Clear</button>
+                  <button type='submit' className='bioBTN'>Submit</button>
+                </div>
+              </form>
+            </div>
+            
           </div>
       </div>
     </>
   )
+}
+
+const loading = () => {
+  return(
+    <>
+      <p>Loading</p>
+    </>
+  ) 
+}
+  return userData ? loaded() : loading();
 }
 
 export default Profile
